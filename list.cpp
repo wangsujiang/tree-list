ba46d4af74196272545c5b227b91d0e7166fe51d
wangsujiang <wangsujiang@outlook.com>
2021-02-01 19:21:42 +0100

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -19,22 +19,9 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-class Solution {
-public:
-    ListNode* swapPairs(ListNode* head) {
-        if (head == nullptr || head->next == nullptr){
-            return head;
-        }
-        ListNode *newhead = head->next;
-        head->next = swapPairs(newhead->next);
-        newhead->next = head;
-        return newhead;
-    }
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 ListNode* newListnode(int val){
@@ -44,12 +31,13 @@ ListNode* newListnode(int val){
     return node;
 }
 
-ListNode* VecToListnode(vector<int> v, int len, int index){
+ListNode* VecToListnode(vector<int> &v, int index){
+    int len = v.size();
     if (index >= len) return nullptr;
 
     ListNode* node = newListnode(v[index]);
     index++;
-    node->next = VecToListnode(v, len, index);
+    node->next = VecToListnode(v, index);
 
     return node;
 }
@@ -63,6 +51,7 @@ void Printlistnode(ListNode* nodehead){
     Printlistnode(nodehead->next);
 }
 
+// INPUT LENGTH KNOWN:
 int main(){
     int n;
     while (cin >> n){
@@ -70,13 +59,37 @@ int main(){
         for (int i=0; i<n; i++){
             cin >> vec[i];
         }
-//        ListNode *node = new ListNode;
-        ListNode* node = VecToListnode(vec, n, 0);
 
-//        ListNode *swapednode = new ListNode;
-        ListNode* swapednode = Solution().swapPairs(node);
+        ListNode* node = VecToListnode(vec, 0);
 
-        Printlistnode(swapednode);
+        //ListNode* swapednode = Solution().swapPairs(node);
+
+        //Printlistnode(swapednode);
     }
     return 0;
+}
+
+
+// INPUT LENGTH UNKNOWN:
+int main(){
+    string input;
+    while (getline(cin, input)){
+        istringstream iss1(input);
+        vector<int> vec1, vec2;
+        int val;
+        while (iss1 >> val){
+            vec1.push_back(val);
+        }
+        getline(cin, input);
+        istringstream iss2(input);
+        while (iss2 >> val){
+            vec2.push_back(val);
+        }
+        ListNode *l1 = VecToListnode(vec1, 0);
+        ListNode *l2 = VecToListnode(vec2, 0);
+
+        //ListNode *ans = mergeTwolists(l1, l2);
+
+        //Printlistnode(ans);
+    }
 }
